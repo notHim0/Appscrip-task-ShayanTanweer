54ab14c1baf12736dabd4296612b9e9eac55ce03
notHim0 <shayantanweer27@gmail.com>
2026-04-22 10:40:17 +0530

    fakestore products

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,8 @@ import "./page.css";
 async function getProducts() {
 	try {
 		const res = await fetch("https://fakestoreapi.com/products", {
-			cache: "no-store",
+			next: { revalidate: 3600 },
 		});
-
 		if (!res.ok) {
 			throw new Error(`API error: ${res.status}`);
 		}
